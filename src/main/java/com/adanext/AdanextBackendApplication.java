1a58dd2bfff1f9394ab63ed5e75d8c28c56ffa8c
Elodie S <elodie.sevestre86@gmail.com>
2026-06-11 14:07:29 +0200

    chore: init project with Spring Initializr

---
+++ src/main/java/com/adanext/AdanextBackendApplication.java
@@ -0,0 +1,13 @@
+package com.adanext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdanextBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdanextBackendApplication.class, args);
+	}
+
+}
