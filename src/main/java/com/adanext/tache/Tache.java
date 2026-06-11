0b9234e7d9d2150913040a9d8f8a846185d945d3
Elodie S <elodie.sevestre86@gmail.com>
2026-06-11 15:14:41 +0200

    feat: create class Tache

--- src/main/java/com/adanext/tache/Tache.java
+++ src/main/java/com/adanext/tache/Tache.java
@@ -1,3 +1,17 @@
+package com.adanext.tache;
+
 public class Tache {
-    
+    private Long id;
+    private String titre;
+    private String statut;
+
+    public Tache(Long id, String titre, String statut) {
+        this.id = id;
+        this.titre = titre;
+        this.statut = statut;
+    }
+
+    public Long getId() { return id; }
+    public String getTitre() { return titre; }
+    public String getStatut() { return statut; }
 }
